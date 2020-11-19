09e0d5fe69eb3e24d483dea11f0e35f0a380aa21
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2020-11-19 21:56:34 +0000

    Create index.java

---
+++ index.java
@@ -0,0 +1,11 @@
+
+package myfirstproject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public static void main(String[] args) {
+    BufferedReader someInputVar = new BufferedReader(new InputStreamReader(System.in));
+         String myName = "Luiza";
+               System.out.println ("Hello," + myName); }}
