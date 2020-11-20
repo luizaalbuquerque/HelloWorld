8fd0c5e640bb8abef9ecf11f93883297bcd8246f
luizaalbuquerque <72030187+luizaalbuquerque@users.noreply.github.com>
2020-11-20 21:29:26 +0000

    Update index.java

--- index.java
+++ index.java
@@ -4,6 +4,7 @@ package myfirstproject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+@luizaAlbuquerque
 
 public static void main(String[] args) {
     BufferedReader someInputVar = new BufferedReader(new InputStreamReader(System.in));
